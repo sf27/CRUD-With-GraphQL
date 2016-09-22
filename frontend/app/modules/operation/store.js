cb68559615ab69a457421030aa6e8068b9e1f867
Elio Rincón <e1987ebrb@gmail.com>
2016-09-22 11:32:41 -0400

    Added logger states

--- frontend/app/modules/operation/store.js
+++ frontend/app/modules/operation/store.js
@@ -1,5 +1,9 @@
-import {createStore} from "redux";
+import thunk from "redux-thunk";
+import {applyMiddleware, compose, createStore} from "redux";
 import OperationsReducer from "./reducers";
+import createLogger from "redux-logger";
 
-const store = createStore(OperationsReducer);
+const logger = createLogger();
+const middlewaresList = [thunk, logger];
+const store = compose(applyMiddleware(...middlewaresList))(createStore)(OperationsReducer);
 export default store;
